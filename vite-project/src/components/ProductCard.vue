bad4dda8c761ebe2b607e13a5715f3f30b726bb7
nathannathan08 <nathanc245@nycstudents.net>
2025-01-30 14:01:59 -0500

    g

---
+++ vite-project/src/components/ProductCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+<h2>{{ product.name }}</h2>
+<img :src="product.image" alt=""/>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+   product:Object,  
+});
+</script>
+
+<style scoped>
+
+</style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,13 +1,27 @@
-<template>
-  <div>
-<NewList />
-  </div>
-</template>
+
 
 <script setup>
-import NewList from '../components/NewList.vue'
+import ProductCard from "../components/ProductCard.vue"
+const products = [
+    {name: 'Laptop', price: 850, rating: 4.6, },
+    {name:'4K Television', price: 1200, rating: 4.7},
+    {name:'Keyboard', price: 40, rating: 4.9},
+    {name:'PC', price: 1100, rating: 4.8},
+    {name:'Wi-Fi Router', price: 150, rating: 4.7},
+    {name:'Room Humidifier', price: 30, rating: 4.4},
+    {name:'4 pack USBs', price: 10, rating: 4.5},
+    {name:'Digital Camera', price: 350, rating: 4.8},
+    {name:'Charging Wire Pack', price: 15, rating: 4.5},
+    {name:'Massage Gun', price: 30, rating: 4.6},
+    {name:'Headphones', price: 300, rating: 4.8},
+    {name:'Printer', price: 160, rating: 4.7},
+];
 </script>
 
 <style scoped>
 
 </style>
+
+<template>
+
+</template>
